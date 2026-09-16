be462f3d1e37846eb41aa12e01aeb96bf5495992
amirdaraby <amir2002.d@gmail.com>
2026-09-16 07:18:52 +0330

    update email view

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -7,7 +7,7 @@ Please find attached your periodic report.
 
 Report Details:
 - Report Name: {{ $report->name }}
-- Frequency: {{ $report->frequency->value === 1 ? 'Daily' : 'Weekly' }}
+- Frequency: {{ $report->frequency === \App\Enums\Frequency::DAILY ? 'Daily' : 'Weekly' }}
 - Keywords: {{ implode(', ', $report->keywords) }}
 
 Thank you.
